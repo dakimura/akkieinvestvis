67e1d697992117cc29c37a5b59529622ee93f8e6
Daito Akimura <dakimura@mercari.com>
2023-12-17 23:18:31 +0900

    fix: add charts

--- my_app.py
+++ my_app.py
@@ -1,9 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.title('Streamlit')
-st.header('Hello World')
-st.text('Hello Streamlit World')
-
+st.title('FX investing performance')
+st.markdown('by [@akkie30](https://twitter.com/akkie30)')
+df = pd.read_csv("data/performance.csv", parse_dates=["Date"], index_col=0)
+st.text("Liquidation Value[JPY]")
+st.line_chart(df["LiquidationValue"])
+st.text("Deposit Balance[JPY]")
+st.line_chart(df["Balance"])
+st.text("Swap Profit[JPY]")
+st.line_chart(df["Swap"])
+st.text("source data")
+st.dataframe(df)
 
 if __name__ == "__main__":
     # $ python -m streamlit run
