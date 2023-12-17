638a6b6bf3a2f5378076b6dc7376aa33152f190d
Daito Akimura <dakimura@mercari.com>
2023-12-17 22:30:38 +0900

    initial commit

---
+++ my_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Streamlit')
+st.header('Hello World')
+st.text('Hello Streamlit World')
+
+
+if __name__ == "__main__":
+    # $ python -m streamlit run
+    pass
