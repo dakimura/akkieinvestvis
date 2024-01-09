e7b62b4ffe931a24cfd41f337180ce64f1771269
Daito Akimura <>
2024-01-09 09:35:16 +0900

    fix: UI

--- my_app.py
+++ my_app.py
@@ -3,14 +3,22 @@ import pandas as pd
 
 st.title('FX investing performance')
 st.markdown('by [@akkie30](https://twitter.com/akkie30)')
+st.image("https://3.bp.blogspot.com/-q3fsc28YHhA/WkR92wRCAZI/AAAAAAABJVo/7R3S9tpX2W8VmcXV40c0NOCZ1Ch2bVgrACLcBGAs/s550/kabu_chart_man_happy.png")
+st.info("2023年7月に500万円で始めたFX投資の成績を可視化する。投資戦略、投資対象通貨ペアは秘密...")
+
 df = pd.read_csv("data/performance.csv", parse_dates=["Date"], index_col=0)
-st.text("Asset[JPY]")
+
+st.metric(label="総資産", value="¥{:,}".format(int(df.tail(1)["Asset"])), delta="¥{:,}".format(int(df.tail(1)["Asset"]) - int(df.head(1)["Asset"])))
+st.metric(label="口座清算価値", value="¥{:,}".format(int(df.tail(1)["LiquidationValue"])), delta="¥{:,}".format(int(df.tail(1)["LiquidationValue"]) - int(df["LiquidationValue"].dropna().head(1))))
+
+
+st.text("総資産 - Asset[JPY]")
 st.line_chart(df["Asset"].dropna())
-st.text("Liquidation Value[JPY]")
+st.text("口座清算価値 - Liquidation Value[JPY]")
 st.line_chart(df["LiquidationValue"].dropna())
-st.text("Swap Profit[JPY]")
+st.text("スワップ利益 - Swap Profit[JPY]")
 st.line_chart(df["Swap"].dropna())
-st.text("source data")
+st.text("元データ - source data")
 st.dataframe(df)
 
 if __name__ == "__main__":
